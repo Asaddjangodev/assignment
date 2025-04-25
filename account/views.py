8ae4b77e83f2133a2ccd484b983a0d241a2ca85a
Asad Khoshimov <hoshimovaustin0550@gmail.com>
2025-04-26 04:43:37 +0900

    - Add authentication views and dashboard. - Implement login_decorator and custom templates for dahsbord view. - Added Django built-in auth views with LoginForm support. - Used built-in login/logout views to protect users from CSRF attacks and provide a secure login process.

--- account/views.py
+++ account/views.py
@@ -2,8 +2,14 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 
 def user_login(request):
